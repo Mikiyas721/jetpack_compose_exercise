935872f1a6dba45715e56255c35aa249fdc4ae36
Mikiyas721 <Mikiyas721@gmail.com>
2022-03-22 11:03:35 -0600

    Added Signuppage 4

--- Signin1/app/src/main/java/com/coolapps/signin1/MainActivity.kt
+++ Signin1/app/src/main/java/com/coolapps/signin1/MainActivity.kt
@@ -3,15 +3,15 @@ package com.coolapps.signin1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material.icons.rounded.Edit
+import androidx.compose.material.icons.rounded.Email
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,11 +19,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.*
@@ -41,7 +43,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
+/**Sign in 1*/
+
 @Composable
 fun SignUpScreen() {
     Column(
@@ -60,7 +63,7 @@ fun SignUpScreen() {
                     .clip(shape = RoundedCornerShape(100))
                     .shadow(elevation = 2.dp)
                     .padding(5.dp)
-                    .clickable {  }
+                    .clickable { }
             ) {
                 Icon(
                     Icons.Rounded.Close,
@@ -206,3 +209,164 @@ fun OutlinedTextFieldWithLabel(
     }
 }
 
+/** Sign in 4*/
+
+@Preview(showBackground = true)
+@Composable
+fun SignupPage() {
+    Column() {
+        Image(
+            painter = painterResource(R.drawable.girl),
+            contentDescription = "Simple Illustration",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(240.dp)
+        )
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp))
+                .padding(bottom = 50.dp)
+
+        ) {
+            Column(modifier = Modifier.padding(horizontal = 20.dp)) {
+                Text(
+                    text = "Log-in",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 28.sp,
+                    modifier = Modifier.padding(vertical = 30.dp)
+                )
+                Column(modifier = Modifier.padding(horizontal = 15.dp)) {
+                    TextFieldWithLabel(
+                        label = "Email",
+                        placeholder = "Your email id",
+                        value = "",
+                        onValueChanged = {},
+                        modifier = Modifier.padding(bottom = 20.dp)
+
+                    )
+                    TextFieldWithLabel(
+                        label = "Password",
+                        placeholder = "Password",
+                        value = "",
+                        onValueChanged = {},
+                        visualTransformation = PasswordVisualTransformation(),
+                        trailingIcon = { Icon(Icons.Rounded.Edit, contentDescription = "") }
+                    )
+                    Row(
+                        horizontalArrangement = Arrangement.End,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        TextButton(onClick = { /*TODO*/ }) {
+                            Text(text = "Forget password?", color = Color.Blue)
+                        }
+                    }
+                    Button(
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF223843)),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(90.dp)
+                            .padding(vertical = 20.dp)
+                            .clip(RoundedCornerShape(40)),
+                        onClick = { /*TODO*/ }) {
+                        Text(text = "Login", color = Color.White, fontSize = 20.sp)
+                    }
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically,
+
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(text = "Don't have an account?", color = Color.Gray)
+                        TextButton(onClick = { /*TODO*/ }) {
+                            Text(text = "Sign-up", color = Color.Blue)
+                        }
+                    }
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 20.dp)
+                    ) {
+                        Divider(
+                            modifier = Modifier
+                                .weight(2f),
+                            thickness = 2.dp
+                        )
+                        Text(
+                            text = "Or login with",
+                            modifier = Modifier
+                                .weight(1.5f),
+                            color = Color.Gray,
+                            textAlign = TextAlign.Center
+                        )
+                        Divider(
+                            modifier = Modifier.weight(2f),
+                            thickness = 2.dp
+                        )
+                    }
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Icon(
+                            Icons.Rounded.Email,
+                            contentDescription = "Login with gmail",
+                            modifier = Modifier
+                                .clip(shape = RoundedCornerShape(100))
+                                .shadow(2.dp, shape = RoundedCornerShape(100))
+                                .padding(10.dp)
+                                .clickable { }
+                        )
+                        Spacer(modifier = Modifier.width(30.dp))
+                        Icon(
+                            Icons.Rounded.Edit,
+                            contentDescription = "Login with facebook",
+                            modifier = Modifier
+                                .clip(shape = RoundedCornerShape(100))
+                                .shadow(2.dp, shape = RoundedCornerShape(100))
+                                .padding(10.dp)
+                                .clickable { }
+                        )
+                    }
+                }
+
+            }
+        }
+
+    }
+}
+
+@Composable
+fun TextFieldWithLabel(
+    label: String,
+    value: String,
+    placeholder: String,
+    onValueChanged: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    trailingIcon: @Composable (() -> Unit) = {}
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = label,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 18.sp,
+            modifier = Modifier.padding(bottom = 5.dp)
+        )
+        TextField(
+            value = value,
+            onValueChange = onValueChanged,
+            placeholder = { Text(text = placeholder) },
+            visualTransformation = visualTransformation,
+            colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.White),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(0.dp)
+                .clip(shape = RoundedCornerShape(10.dp)),
+            trailingIcon = trailingIcon
+        )
+    }
+}
